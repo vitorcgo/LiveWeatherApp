ca2e9abf1d3adcd62709c34db04ae193d8195424
Vitor <zbro2018@gmail.com>
2026-09-15 22:14:26 -0300

    refactor: modularize weather app architecture

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WheatherApp from './components/WheatherApp'
+import './components/WheatherApp.css'
+
+const App = () => {
+  return (
+    <div>
+      <WheatherApp />
+    </div>
+  )
+}
+
+export default App
