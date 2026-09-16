508c055f79d8e580d60dbab7b9a2667e99d51084
Vitor <zbro2018@gmail.com>
2026-09-15 21:08:41 -0300

    feat: control weather location input

--- liveweatherapp/src/App.jsx
+++ liveweatherapp/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import WheatherApp from './components/WheatherApp'
+import './components/WheatherApp.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      
-    </>
+    <div>
+      <WheatherApp />
+    </div>
   )
 }
 
